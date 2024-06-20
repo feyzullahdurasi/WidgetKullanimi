1c544ca83c67155e09e72eabe0983f2b25965e89
Feyzullah Durası <durasifeyzullah672@gmail.com>
2024-06-20 17:05:52 +0300

    Initial Commit

---
+++ WidgetKullanimi/WidgetKullanimiApp.swift
@@ -0,0 +1,17 @@
+//
+//  WidgetKullanimiApp.swift
+//  WidgetKullanimi
+//
+//  Created by Feyzullah Durası on 20.06.2024.
+//
+
+import SwiftUI
+
+@main
+struct WidgetKullanimiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
